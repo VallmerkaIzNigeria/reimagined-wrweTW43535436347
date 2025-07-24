24bd90336eff9865525292de6b097bc20ce76258
VallmerkaIzNigeria <vallkcorp@gmail.com>
2025-07-24 14:23:47 +0700

    Update PasteWare.lua

--- PasteWare.lua
+++ PasteWare.lua
@@ -313,9 +313,9 @@ end)
 
 
 
-local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/FakeAngles/PasteWare/refs/heads/main/linoralib.lua"))()
-local ThemeManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/FakeAngles/PasteWare/refs/heads/main/manage2.lua"))()
-local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/FakeAngles/PasteWare/refs/heads/main/manager.lua"))()
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/VallmerkaIzNigeria/reimagined-wrweTW43535436347/refs/heads/main/linoralib.lua"))()
+local ThemeManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/VallmerkaIzNigeria/reimagined-wrweTW43535436347/refs/heads/main/manage2.lua"))()
+local SaveManager = loadstring(game:HttpGet("https://raw.githubusercontent.com/VallmerkaIzNigeria/reimagined-wrweTW43535436347/refs/heads/main/manager.lua"))()
 
 Library.KeybindFrame.Visible = true;
 
